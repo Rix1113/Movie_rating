cda31349fc6b16d7202107c415475e5f03305db2
Rix <80359407+Rix1113@users.noreply.github.com>
2021-11-03 17:26:53 +0200

    Add Spring security, using deprecated method but I just want to try.

---
+++ src/main/java/com/sda/moviedb/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.sda.moviedb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // add our users for in memory authentication
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("Employee").password("123").roles("EMPLOYEE"))
+                .withUser(users.username("Manager").password("123").roles("MANAGER"))
+                .withUser(users.username("Admin").password("123").roles("ADMIN"));
+
+         }
+}
---
+++ src/main/java/com/sda/moviedb/config/SecurityWebAppInitializer.java
@@ -0,0 +1,7 @@
+package com.sda.moviedb.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
